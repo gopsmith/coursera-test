7f82d7069ed7a06dd2cd6d7f8a41676d0b58be96
Gordon P. Smith <gosmith@nyc.rr.com>
2017-02-03 15:14:52 -0500

    first commit of module1-solution

---
+++ module1-solution/js/app.js
@@ -0,0 +1,31 @@
+(function () {
+  'user strict';
+
+  angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope', '$filter'];
+  function LunchCheckController($scope, $filter) {
+    $scope.lunchItems = "";
+    $scope.message = "";
+    $scope.messageFeedback= "";
+    $scope.inputFeedback = "";
+
+    $scope.checkIfTooMuch = function() {
+      if ($scope.lunchItems == "") {
+        $scope.message = "Please enter data first"
+        $scope.messageFeedback= "bad-text"
+        $scope.inputFeedback = "bad-border"
+      }
+      else {
+        var numItems = $scope.lunchItems
+          .split(",")
+          .filter( function(val) {return val != ""} )
+          .length;
+        $scope.message = (numItems <=3 ? "Enjoy!" : "Too much!");
+        $scope.messageFeedback = "good-text"
+        $scope.inputFeedback = "good-border"
+      }
+    }
+  }
+})();
