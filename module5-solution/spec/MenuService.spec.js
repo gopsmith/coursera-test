e6a7cbe20448aa05885c8136fc4df83d1bfe3220
Gordon P. Smith <gosmith@nyc.rr.com>
2017-03-07 08:32:26 -0500

    Fixed Jasmine tests

--- module5-solution/spec/MenuService.spec.js
+++ module5-solution/spec/MenuService.spec.js
@@ -5,45 +5,40 @@ describe('getMenuItem', function () {
   var ApiPath;
 
   beforeEach(function () {
-    module('common'
-    //this was an attempt to inject $httpProvider another way. it didn't cause any errors in itself
-      , function (_$httpProvider_) {
-        //save our interceptor
-        $httpProvider = _$httpProvider_;
-      }
-    );
+    module('common');
 
     inject(function ($injector) {
-      // this first $injector call, uncommented, causes the following error:
-      // http://errors.angularjs.org/1.5.8/$injector/unpr?p0=loadingHttpInterceptorProvider%20%3C-%20loadingHttpInterceptor%20%3C-%20%24http%20%3C-%20MenuService
       menuService = $injector.get('MenuService');
       $httpBackend = $injector.get('$httpBackend');
       ApiPath = $injector.get('ApiPath');
-      // none of these work, they all cause errors:
-      // $http = $injector.get('$http');
-      // loadingHttpInterceptor = $injector.get('loadingHttpInterceptor');
-      // loadingHttpInterceptorProvider = $injector.get('loadingHttpInterceptorProvider');
-      // $rootScope = $injector.get('$rootScope');
-      // $q = $injector.get('$q');
     });
   });
 
   it('should return categories list', function() {
-    expect(1).toEqual(1);   //just trying to get the $injector calls working, first...
-    // $httpBackend.whenGET(ApiPath + '/categories.json').respond(['Lunch', 'Dessert']);
-    // menuService.getCategories().then(function(data) {
-    //   expect(data).toEqual(['Lunch', 'Dessert']);
-    // });
-    // $httpBackend.flush();
+    $httpBackend.whenGET(ApiPath + '/categories.json').respond([
+      {id: 1, short_name: 'L', name: 'Lunch', special_instructions: 'special instrs', url: 'http://www.whatev.com'},
+      {id: 2, short_name: 'A', name: 'Soup', special_instructions: 'specialinstr', url: 'http://goodtimes.com'}
+    ]);
+    menuService.getCategories().then(function(data) {
+      expect(data).toEqual([
+        {id: 1, short_name: 'L', name: 'Lunch', special_instructions: 'special instrs', url: 'http://www.whatev.com'},
+        {id: 2, short_name: 'A', name: 'Soup', special_instructions: 'specialinstr', url: 'http://goodtimes.com'}
+      ]);
+    });
+    $httpBackend.flush();
   });
-  //
-  // it('should return a valid menu item', function() {
-  //   var itemCode = 'A1';
-  //   $httpBackend.whenGET(ApiPath + '/menu_items/' + itemCode + '.json').respond(['Lunch', 'Dessert']);
-  //   menuService.getMenuItem(itemCode).then(function(response) {
-  //     expect(response.data).toEqual(['Lunch', 'Dessert']);
-  //   });
-  //   $httpBackend.flush();
-  // });
+
+  it('should return a valid menu item', function() {
+    var itemCode = 'A2';
+    $httpBackend.whenGET(ApiPath + '/menu_items/' + itemCode + '.json').respond([
+      {id: 2, short_name: 'A2', name: 'Egg Drop Soup', description: 'chicken broth with egg drop'}
+    ]);
+    menuService.getMenuItem(itemCode).then(function(data) {
+      expect(data).toEqual([
+        {id: 2, short_name: 'A2', name: 'Egg Drop Soup', description: 'chicken broth with egg drop'}
+      ]);
+    });
+    $httpBackend.flush();
+  });
 
 });
--- module5-solution/src/common/common.module.js
+++ module5-solution/src/common/common.module.js
@@ -5,7 +5,6 @@ angular.module('common', [])
 .constant('ApiPath', 'https://gps-coursera-angular.herokuapp.com')
 .config(config);
 
-//if I comment out this last section, then the first Jasmine test works fine:
 config.$inject = ['$httpProvider'];
 function config($httpProvider) {
   $httpProvider.interceptors.push('loadingHttpInterceptor');
